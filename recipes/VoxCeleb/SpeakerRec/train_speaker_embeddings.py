827599fb995770ae546c9f9dfd7a868d71d86785
Max Dudik <maxdudik@gmail.com>
2024-08-16 21:35:34 +0700

    Update train_speaker_embeddings.py

--- recipes/VoxCeleb/SpeakerRec/train_speaker_embeddings.py
+++ recipes/VoxCeleb/SpeakerRec/train_speaker_embeddings.py
@@ -1,190 +1,244 @@
-# ################################
-# Model: Speaker identification with ECAPA
-# Authors: Hwidong Na & Mirco Ravanelli
-# ################################
+#!/usr/bin/python3
+"""Recipe for training speaker embeddings (e.g, xvectors) using the VoxCeleb Dataset.
+We employ an encoder followed by a speaker classifier.
 
-# Basic parameters
-seed: 1986
-__set_seed: !apply:torch.manual_seed [!ref <seed>]
-output_folder: !ref results/ecapa_augment/<seed>
-save_folder: !ref <output_folder>/save
-train_log: !ref <output_folder>/train_log.txt
+To run this recipe, use the following command:
+> python train_speaker_embeddings.py {hyperparameter_file}
 
-# Data for augmentation
-NOISE_DATASET_URL: https://www.dropbox.com/scl/fi/a09pj97s5ifan81dqhi4n/noises.zip?rlkey=j8b0n9kdjdr32o1f06t0cw5b7&dl=1
-RIR_DATASET_URL: https://www.dropbox.com/scl/fi/linhy77c36mu10965a836/RIRs.zip?rlkey=pg9cu8vrpn2u173vhiqyu743u&dl=1
+Using your own hyperparameter file or one of the following:
+    hyperparams/train_x_vectors.yaml (for standard xvectors)
+    hyperparams/train_ecapa_tdnn.yaml (for the ecapa+tdnn system)
 
-# Data files
-data_folder: ../../../dataset/dataset/vox_dnd  # e.g. /path/to/Voxceleb
-data_folder_noise: !ref <data_folder>/noise # The noisy sequences for data augmentation will automatically be downloaded here.
-data_folder_rir: !ref <data_folder>/rir # The impulse responses used for data augmentation will automatically be downloaded here.
-train_annotation: !ref <save_folder>/train.csv
-valid_annotation: !ref <save_folder>/dev.csv
-noise_annotation: !ref <save_folder>/noise.csv
-rir_annotation: !ref <save_folder>/rir.csv
+Author
+    * Mirco Ravanelli 2020
+    * Hwidong Na 2020
+    * Nauman Dawalatabad 2020
+"""
+import os
+import random
+import sys
 
-# Use the following links for the official voxceleb splits:
-# VoxCeleb1 (cleaned): https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/veri_test2.txt
-# VoxCeleb1-H (cleaned): https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/list_test_hard2.txt
-# VoxCeleb1-E (cleaned): https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/list_test_all2.txt.
-# VoxCeleb1-E and VoxCeleb1-H lists are drawn from the VoxCeleb1 training set.
-# Therefore you cannot use any files in VoxCeleb1 for training if you are using these lists for testing.
-verification_file: https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/veri_test2.txt
+import torch
+import torchaudio
+from hyperpyyaml import load_hyperpyyaml
 
-split_ratio: [90, 10]
-skip_prep: False
-ckpt_interval_minutes: 15 # save checkpoint every N min
-
-# Training parameters
-number_of_epochs: 10
-batch_size: 16
-lr: 0.001
-base_lr: 0.00000001
-max_lr: !ref <lr>
-step_size: 65000
-sample_rate: 16000
-sentence_len: 3.0 # seconds
-shuffle: True
-random_chunk: True
-
-# Feature parameters
-n_mels: 80
-left_frames: 0
-right_frames: 0
-deltas: False
-
-# Number of speakers
-out_n_neurons: 27 #1211 for vox1  # 5994 for vox2, 7205 for vox1+vox2
-
-num_workers: 2
-dataloader_options:
-    batch_size: !ref <batch_size>
-    shuffle: !ref <shuffle>
-    num_workers: !ref <num_workers>
-
-# Functions
-compute_features: !new:speechbrain.lobes.features.Fbank
-    n_mels: !ref <n_mels>
-    left_frames: !ref <left_frames>
-    right_frames: !ref <right_frames>
-    deltas: !ref <deltas>
-
-embedding_model: !new:speechbrain.lobes.models.ECAPA_TDNN.ECAPA_TDNN
-    input_size: !ref <n_mels>
-    channels: [1024, 1024, 1024, 1024, 3072]
-    kernel_sizes: [5, 3, 3, 3, 1]
-    dilations: [1, 2, 3, 4, 1]
-    groups: [1, 1, 1, 1, 1]
-    attention_channels: 128
-    lin_neurons: 192
-
-classifier: !new:speechbrain.lobes.models.ECAPA_TDNN.Classifier
-    input_size: 192
-    out_neurons: !ref <out_n_neurons>
-
-epoch_counter: !new:speechbrain.utils.epoch_loop.EpochCounter
-    limit: !ref <number_of_epochs>
-
-############################## Augmentations ###################################
-
-# Download and prepare the dataset of noisy sequences for augmentation
-prepare_noise_data: !name:speechbrain.augment.preparation.prepare_dataset_from_URL
-    URL: !ref <NOISE_DATASET_URL>
-    dest_folder: !ref <data_folder_noise>
-    ext: wav
-    csv_file: !ref <noise_annotation>
+import speechbrain as sb
+from speechbrain.utils.data_utils import download_file
+from speechbrain.utils.distributed import run_on_main
 
 
-# Add noise to input signal
-add_noise: !new:speechbrain.augment.time_domain.AddNoise
-    csv_file: !ref <noise_annotation>
-    snr_low: 0
-    snr_high: 15
-    noise_sample_rate: !ref <sample_rate>
-    clean_sample_rate: !ref <sample_rate>
-    num_workers: !ref <num_workers>
+class SpeakerBrain(sb.core.Brain):
+    """Class for speaker embedding training"""
 
-# Download and prepare the dataset of room impulse responses for augmentation
-prepare_rir_data: !name:speechbrain.augment.preparation.prepare_dataset_from_URL
-    URL: !ref <RIR_DATASET_URL>
-    dest_folder: !ref <data_folder_rir>
-    ext: wav
-    csv_file: !ref <rir_annotation>
+    def compute_forward(self, batch, stage):
+        """Computation pipeline based on a encoder + speaker classifier.
+        Data augmentation and environmental corruption are applied to the
+        input speech.
+        """
+        batch = batch.to(self.device)
+        wavs, lens = batch.sig
 
-# Add reverberation to input signal
-add_reverb: !new:speechbrain.augment.time_domain.AddReverb
-    csv_file: !ref <rir_annotation>
-    reverb_sample_rate: !ref <sample_rate>
-    clean_sample_rate: !ref <sample_rate>
-    num_workers: !ref <num_workers>
+        # Add waveform augmentation if specified.
+        if stage == sb.Stage.TRAIN and hasattr(self.hparams, "wav_augment"):
+            wavs, lens = self.hparams.wav_augment(wavs, lens)
 
-# Frequency drop: randomly drops a number of frequency bands to zero.
-drop_freq: !new:speechbrain.augment.time_domain.DropFreq
-    drop_freq_low: 0
-    drop_freq_high: 1
-    drop_freq_count_low: 1
-    drop_freq_count_high: 3
-    drop_freq_width: 0.05
+        # Feature extraction and normalization
+        if (
+            hasattr(self.hparams, "use_tacotron2_mel_spec")
+            and self.hparams.use_tacotron2_mel_spec
+        ):
+            feats = self.hparams.compute_features(audio=wavs)
+            feats = torch.transpose(feats, 1, 2)
+        else:
+            feats = self.modules.compute_features(wavs)
+        feats = self.modules.mean_var_norm(feats, lens)
 
-# Time drop: randomly drops a number of temporal chunks.
-drop_chunk: !new:speechbrain.augment.time_domain.DropChunk
-    drop_length_low: 1000
-    drop_length_high: 2000
-    drop_count_low: 1
-    drop_count_high: 5
+        # Embeddings + speaker classifier
+        embeddings = self.modules.embedding_model(feats)
+        outputs = self.modules.classifier(embeddings)
 
-# Augmenter: Combines previously defined augmentations to perform data augmentation
-wav_augment: !new:speechbrain.augment.augmenter.Augmenter
-    parallel_augment: True
-    concat_original: True
-    min_augmentations: 4
-    max_augmentations: 4
-    augment_prob: 1.0
-    augmentations: [
-        !ref <add_noise>,
-        !ref <add_reverb>,
-        !ref <drop_freq>,
-        !ref <drop_chunk>]
+        return outputs, lens
 
-mean_var_norm: !new:speechbrain.processing.features.InputNormalization
-    norm_type: sentence
-    std_norm: False
+    def compute_objectives(self, predictions, batch, stage):
+        """Computes the loss using speaker-id as label."""
+        predictions, lens = predictions
+        uttid = batch.id
+        spkid, _ = batch.spk_id_encoded
 
-modules:
-    compute_features: !ref <compute_features>
-    embedding_model: !ref <embedding_model>
-    classifier: !ref <classifier>
-    mean_var_norm: !ref <mean_var_norm>
+        # Concatenate labels (due to data augmentation)
+        if stage == sb.Stage.TRAIN and hasattr(self.hparams, "wav_augment"):
+            spkid = self.hparams.wav_augment.replicate_labels(spkid)
 
-compute_cost: !new:speechbrain.nnet.losses.LogSoftmaxWrapper
-    loss_fn: !new:speechbrain.nnet.losses.AdditiveAngularMargin
-        margin: 0.2
-        scale: 30
+        loss = self.hparams.compute_cost(predictions, spkid, lens)
 
-# compute_error: !name:speechbrain.nnet.losses.classification_error
+        if stage == sb.Stage.TRAIN and hasattr(
+            self.hparams.lr_annealing, "on_batch_end"
+        ):
+            self.hparams.lr_annealing.on_batch_end(self.optimizer)
 
-opt_class: !name:torch.optim.Adam
-    lr: !ref <lr>
-    weight_decay: 0.000002
+        if stage != sb.Stage.TRAIN:
+            self.error_metrics.append(uttid, predictions, spkid, lens)
 
-lr_annealing: !new:speechbrain.nnet.schedulers.CyclicLRScheduler
-    base_lr: !ref <base_lr>
-    max_lr: !ref <max_lr>
-    step_size: !ref <step_size>
+        return loss
 
-# Logging + checkpoints
-train_logger: !new:speechbrain.utils.train_logger.FileTrainLogger
-    save_file: !ref <train_log>
+    def on_stage_start(self, stage, epoch=None):
+        """Gets called at the beginning of an epoch."""
+        if stage != sb.Stage.TRAIN:
+            self.error_metrics = self.hparams.error_stats()
 
-error_stats: !name:speechbrain.utils.metric_stats.MetricStats
-    metric: !name:speechbrain.nnet.losses.classification_error
-        reduction: batch
+    def on_stage_end(self, stage, stage_loss, epoch=None):
+        """Gets called at the end of an epoch."""
+        # Compute/store important stats
+        stage_stats = {"loss": stage_loss}
+        if stage == sb.Stage.TRAIN:
+            self.train_stats = stage_stats
+        else:
+            stage_stats["ErrorRate"] = self.error_metrics.summarize("average")
 
-checkpointer: !new:speechbrain.utils.checkpoints.Checkpointer
-    checkpoints_dir: !ref <save_folder>
-    recoverables:
-        embedding_model: !ref <embedding_model>
-        classifier: !ref <classifier>
-        normalizer: !ref <mean_var_norm>
-        counter: !ref <epoch_counter>
-        lr_annealing: !ref <lr_annealing>
+        # Perform end-of-iteration things, like annealing, logging, etc.
+        if stage == sb.Stage.VALID:
+            old_lr, new_lr = self.hparams.lr_annealing(epoch)
+            sb.nnet.schedulers.update_learning_rate(self.optimizer, new_lr)
+
+            self.hparams.train_logger.log_stats(
+                stats_meta={"epoch": epoch, "lr": old_lr},
+                train_stats=self.train_stats,
+                valid_stats=stage_stats,
+            )
+            self.checkpointer.save_and_keep_only(
+                meta={"ErrorRate": stage_stats["ErrorRate"]},
+                min_keys=["ErrorRate"],
+            )
+
+
+def dataio_prep(hparams):
+    "Creates the datasets and their data processing pipelines."
+
+    data_folder = hparams["data_folder"]
+
+    # 1. Declarations:
+    train_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["train_annotation"],
+        replacements={"data_root": data_folder},
+    )
+
+    valid_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["valid_annotation"],
+        replacements={"data_root": data_folder},
+    )
+
+    datasets = [train_data, valid_data]
+    label_encoder = sb.dataio.encoder.CategoricalEncoder()
+
+    snt_len_sample = int(hparams["sample_rate"] * hparams["sentence_len"])
+
+    # 2. Define audio pipeline:
+    @sb.utils.data_pipeline.takes("wav", "start", "stop", "duration")
+    @sb.utils.data_pipeline.provides("sig")
+    def audio_pipeline(wav, start, stop, duration):
+        if hparams["random_chunk"]:
+            duration_sample = int(duration * hparams["sample_rate"])
+            start = random.randint(0, duration_sample - snt_len_sample)
+            stop = start + snt_len_sample
+        else:
+            start = int(start)
+            stop = int(stop)
+        num_frames = stop - start
+        sig, fs = torchaudio.load(
+            wav, num_frames=num_frames, frame_offset=start
+        )
+        sig = sig.transpose(0, 1).squeeze(1)
+        return sig
+
+    sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline)
+
+    # 3. Define text pipeline:
+    @sb.utils.data_pipeline.takes("spk_id")
+    @sb.utils.data_pipeline.provides("spk_id", "spk_id_encoded")
+    def label_pipeline(spk_id):
+        yield spk_id
+        spk_id_encoded = label_encoder.encode_sequence_torch([spk_id])
+        yield spk_id_encoded
+
+    sb.dataio.dataset.add_dynamic_item(datasets, label_pipeline)
+
+    # 3. Fit encoder:
+    # Load or compute the label encoder (with multi-GPU DDP support)
+    lab_enc_file = os.path.join(hparams["save_folder"], "label_encoder.txt")
+    label_encoder.load_or_create(
+        path=lab_enc_file,
+        from_didatasets=[train_data],
+        output_key="spk_id",
+    )
+
+    # 4. Set output:
+    sb.dataio.dataset.set_output_keys(datasets, ["id", "sig", "spk_id_encoded"])
+
+    return train_data, valid_data, label_encoder
+
+
+if __name__ == "__main__":
+    # This flag enables the inbuilt cudnn auto-tuner
+    torch.backends.cudnn.benchmark = True
+
+    # CLI:
+    hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
+
+    # Initialize ddp (useful only for multi-GPU DDP training)
+    sb.utils.distributed.ddp_init_group(run_opts)
+
+    # Load hyperparameters file with command-line overrides
+    with open(hparams_file) as fin:
+        hparams = load_hyperpyyaml(fin, overrides)
+
+    # Download verification list (to exclude verification sentences from train)
+    veri_file_path = os.path.join(
+        hparams["save_folder"], os.path.basename(hparams["verification_file"])
+    )
+    download_file(hparams["verification_file"], veri_file_path)
+
+    # Dataset prep (parsing VoxCeleb and annotation into csv files)
+    from voxceleb_prepare import prepare_voxceleb  # noqa
+
+    run_on_main(
+        prepare_voxceleb,
+        kwargs={
+            "data_folder": hparams["data_folder"],
+            "save_folder": hparams["save_folder"],
+            "verification_pairs_file": veri_file_path,
+            "splits": ["train", "dev"],
+            "split_ratio": hparams["split_ratio"],
+            "seg_dur": hparams["sentence_len"],
+            "skip_prep": hparams["skip_prep"],
+        },
+    )
+    sb.utils.distributed.run_on_main(hparams["prepare_noise_data"])
+    sb.utils.distributed.run_on_main(hparams["prepare_rir_data"])
+
+    # Dataset IO prep: creating Dataset objects and proper encodings for phones
+    train_data, valid_data, label_encoder = dataio_prep(hparams)
+
+    # Create experiment directory
+    sb.core.create_experiment_directory(
+        experiment_directory=hparams["output_folder"],
+        hyperparams_to_save=hparams_file,
+        overrides=overrides,
+    )
+
+    # Brain class initialization
+    speaker_brain = SpeakerBrain(
+        modules=hparams["modules"],
+        opt_class=hparams["opt_class"],
+        hparams=hparams,
+        run_opts=run_opts,
+        checkpointer=hparams["checkpointer"],
+    )
+
+    # Training
+    speaker_brain.fit(
+        speaker_brain.hparams.epoch_counter,
+        train_data,
+        valid_data,
+        train_loader_kwargs=hparams["dataloader_options"],
+        valid_loader_kwargs=hparams["dataloader_options"],
+    )
